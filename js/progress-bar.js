781e864dd279b3ec0daf9066babe10416ae0193a
Dephiz <you@example.com>
2019-09-03 14:01:43 +0500

    complete progress

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -3,12 +3,16 @@ let progressText = $('#progress-text');
 // Переменные для проверки изменения инпутов
 let sumChanged = false;
 let monthChanged = false;
+let zalogChanged = false;
+let zalogItemChanged = false;
+let nameChanged = false;
+let phoneChanged = false;
 
 // Начальное значение процента
 let currentPercent = 10;
 
 
-$('#range-sum').on('change', function(){
+$('#range-sum').on('input', function(){
   if(!sumChanged){
     sumChanged = true;
     progressAddPercent(23);
@@ -20,17 +24,67 @@ $('#range-sum').on('change', function(){
   }
 });
 
-$('#range-time').on('change', function(){
+$('#range-time').on('input', function(){
   if(!monthChanged){
     monthChanged = true;
     progressAddPercent(23);
+  } else {
+    if($(this).val() == 0){
+      monthChanged = false;
+      progressSubtractPercent(23);
+    }
   }
 });
 
-// функция получения ширины элемента в процентах
-$.fn.percWidth = function(){
-  return this.outerWidth() / this.parent().outerWidth() * 100;
-}
+$('#type-select').on('change', function(){
+  if(!zalogChanged){
+    zalogChanged = true;
+    progressAddPercent(4);
+  }
+});
+
+$('#home-input').on('change', function(){
+  if(!zalogItemChanged){
+    zalogItemChanged = true;
+    progressAddPercent(14);
+  }
+});
+
+$('#car-input').on('input', function(){
+  if(!zalogItemChanged){
+    zalogItemChanged = true;
+    progressAddPercent(14);
+  } else {
+    if($(this).val() == ''){
+      zalogItemChanged = false;
+      progressSubtractPercent(14);
+    }
+  }
+});
+
+$('#name-input').on('input', function(){
+  if(!nameChanged){
+    nameChanged = true;
+    progressAddPercent(3);
+  } else {
+    if($(this).val() == ''){
+      nameChanged = false;
+      progressSubtractPercent(3);
+    }
+  }
+});
+
+$('#phone-input').on('input', function(){
+  if(!phoneChanged){
+    phoneChanged = true;
+    progressAddPercent(3);
+  } else {
+    if($(this).val() == ''){
+      phoneChanged = false;
+      progressSubtractPercent(3);
+    }
+  }
+});
 
 function progressAddPercent(count){
   let progressLine = $('.calculator-progress__line');
