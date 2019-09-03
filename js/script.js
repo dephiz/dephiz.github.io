844f7abe03577c7c0b55803c9a56f9912c6a2ffc
Dephiz <you@example.com>
2019-09-03 16:42:11 +0500

    fix steps

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let currentTab = 0;
-let maxTub = 2;
+let maxTub = 3;
 let steps = document.getElementsByClassName("step");
 let btn = document.querySelector('#next-btn');
 
@@ -11,9 +11,32 @@ function showTab(n) {
 
 function next() {
   if(++currentTab <= maxTub){
-    if(currentTab === 2){
-      addInfoToCard();
+    if(currentTab == 1){
+      if(StepOneHasError()){
+        --currentTab;
+        return;
+      }
+      progressAddPercent(9);
     }
+
+    if(currentTab == 2){
+      if(StepTwoHasError()){
+        --currentTab;
+        return;
+      }
+      addInfoToCard();
+      progressAddPercent(1);
+    }
+
+    if(currentTab == 3){
+      if(StepThreeHasError()){
+        --currentTab;
+        return;
+      }
+
+      $('.calculator-step').text('Все шаги пройдены');
+    }
+
     showTab(currentTab);
   }
 }
@@ -34,6 +57,13 @@ function fixStepIndicator(n) {
 
   if(n == 2){
     $('#calculator-up-percents').text('3% за ввод ФИО');
+    $('#next-btn').text('Отправить');
+  } else {
+    $('#next-btn').text('Далее');
+  }
+
+  if(n == 3){
+    $('#calculator-up-percents').text('');
   }
 }
 
@@ -41,4 +71,62 @@ function addInfoToCard(){
     card = $('#card1').html();
     $('#prev-info').empty();
     $('#prev-info').append(card);
+}
+
+function StepOneHasError(){
+  let error = false;
+  let inputs = $('.calculator__range');
+  for (var i = 0; i < inputs.length; i++) {
+    if($(inputs[i]).val() == 0){
+      error = true;
+    }
+  }
+
+  if (error){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function StepTwoHasError(){
+  let error = false;
+
+  if($('#type-select').val() == null){
+    error = true;
+  } else {
+    if($('#type-select').val() == 'pts'){
+      if($('#car-input').val() == ''){
+        error = true;
+      }
+    } else {
+      if($('#home-input').val() == null){
+        error = true;
+      }
+    }
+  }
+
+  if (error){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function StepThreeHasError(){
+  let error = false;
+
+  if($('#name-input').val() == ''){
+    error = true;
+  }
+
+  if($('#phone-input').val() == ''){
+    error = true;
+  }
+
+  if (error){
+    return true;
+  } else {
+    return false;
+  }
 }
